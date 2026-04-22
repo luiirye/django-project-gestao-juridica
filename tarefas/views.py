24d0f59844907727016060d1e428a0af8402c3d4
luii <borg.esluii01645@gmail.com>
2026-04-22 18:02:26 -0400

    Templates:, template home

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render #importar a função render para renderizar templates HTML
 from django.http import HttpResponse
 
 # Create your views here.
 
 def tarefas_home(request):
-    return HttpResponse("<h1>Aqui estão suas tarefas listadas</h1>")
+    # é possível passar variáveis para o template HTML através de um dicionário
+    contexto = {
+        "nome" : "Luis Felipe",
+    }
+    
+    return render(request, 'tarefas/home.html', contexto)
 
 def registrar_tarefas(request):
     return HttpResponse("<h1>Registre aqui uma nova tarefa</h1>")
